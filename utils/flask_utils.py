4fa6dd2522d3ca410722d942e5ffab9515c4d492
Serj Mileev <smileev@remedypointsolutions.com>
2025-01-17 17:52:13 +0300

    Update flask_utils.py

--- utils/flask_utils.py
+++ utils/flask_utils.py
@@ -8,7 +8,7 @@ class __FlaskUtils(Singleton):
     def __init__(self):
         self.app = Flask(__name__)
 
-        self.api = FlaskPydanticSpec('flask', title='Item API')
+        self.api = FlaskPydanticSpec('flask', title='SolHealth API')
         self.api.register(self.app)
 
 flask = __FlaskUtils()
