17f76ef7895e354fcda08a6136f89260e89f0925
Serj Mileev <smileev@remedypointsolutions.com>
2025-02-05 20:26:03 +0300

    adding tables

--- src/db/database.py
+++ src/db/database.py
@@ -41,6 +41,9 @@ class _Database(Singleton):
 
         engine = create_engine(url, connect_args=args)
 
+        for table in Base.metadata.tables.values():
+            table.create(engine, checkfirst=True)
+
         session_maker = sessionmaker(bind=engine)
         self.session = session_maker()
 
