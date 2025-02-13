d872351be717a1392dc5a13249db1b4a5da215b6
Serj Mileev <smileev@remedypointsolutions.com>
2025-02-13 17:12:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,10 @@ app.register_api(therapist_api)
 
 @app.after_request
 def set_cors_headers(response):
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
-    response.headers['Access-Control-Allow-Origin'] = 'https://*.solhealth.co'
+    origin = request.headers.get('Origin')
+    if origin and origin.endswith("solhealth.co"):
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+        response.headers['Access-Control-Allow-Origin'] = origin
     return response
 
 
