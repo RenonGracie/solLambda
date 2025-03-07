52a248826c3bd1b79074a6b42281494e0d3a335b
Serj Mileev <smileev@remedypointsolutions.com>
2025-03-07 17:43:30 +0300

    Shd 103 sync filtered therapists calendars with g calendar before showing to client needed in case hook from intake q calendar wasnt received (#33)
    * Update match.py
    
    * SHD-103 update logic
    
    * fix

--- src/utils/matching_algorithm/match.py
+++ src/utils/matching_algorithm/match.py
@@ -33,46 +33,66 @@ def match_client_with_therapists(
             if len(caseload) > 0:
                 max_caseload = int(caseload[-1])
                 if max_caseload > 0:
-                    first_week_appointments, second_week_appointments = (
-                        get_appointments_for_therapist(db, therapist)
+                    matches.append(
+                        {
+                            "therapist": therapist,
+                            "score": score,
+                            "matched_specialities": matched_specialities,
+                            "matched_diagnoses": matched_diagnoses,
+                            "max_caseload": max_caseload,
+                        }
                     )
-                    first_week_client_emails = {
-                        appointment.client_email
-                        for appointment in first_week_appointments
-                    }
-                    second_week_client_emails = {
-                        appointment.client_email
-                        for appointment in second_week_appointments
-                    }
-
-                    if len(first_week_client_emails) > max_caseload:
-                        first_week_appointments = None
-                    if len(second_week_client_emails) > max_caseload:
-                        second_week_appointments = None
-
-                    if (
-                        first_week_appointments is not None
-                        or second_week_appointments is not None
-                    ):
-                        therapist = provide_therapist_slots(
-                            therapist,
-                            first_week_appointments,
-                            second_week_appointments,
-                        )
-                        if len(therapist.available_slots) > 0:
-                            matches.append(
-                                {
-                                    "therapist": therapist,
-                                    "score": score,
-                                    "matched_specialities": matched_specialities,
-                                    "matched_diagnoses": matched_diagnoses,
-                                }
-                            )
 
     matches = implement_age_factor(
         form.age,
         sorted(
             matches,
+            key=lambda i: i.get("score"),
+            reverse=True,
+        ),
+    )
+
+    matched_therapists = []
+    for match in matches:
+        therapist = match["therapist"]
+        max_caseload = match["max_caseload"]
+        first_week_appointments, second_week_appointments = (
+            get_appointments_for_therapist(db, therapist)
+        )
+        first_week_client_emails = {
+            appointment.client_email for appointment in first_week_appointments
+        }
+        second_week_client_emails = {
+            appointment.client_email for appointment in second_week_appointments
+        }
+
+        if len(first_week_client_emails) > max_caseload:
+            first_week_appointments = None
+        if len(second_week_client_emails) > max_caseload:
+            second_week_appointments = None
+
+        if first_week_appointments is not None or second_week_appointments is not None:
+            therapist = provide_therapist_slots(
+                therapist,
+                first_week_appointments,
+                second_week_appointments,
+            )
+            if len(therapist.available_slots) > 0:
+                matched_therapists.append(
+                    {
+                        "therapist": therapist,
+                        "score": match["score"],
+                        "matched_specialities": match["matched_specialities"],
+                        "matched_diagnoses": match["matched_diagnoses"],
+                    }
+                )
+        if len(matched_therapists) == limit + last_index:
+            break
+
+    matched_therapists = implement_age_factor(
+        form.age,
+        sorted(
+            matched_therapists,
             key=lambda i: (i.get("score"), len(i["therapist"].available_slots)),
             reverse=True,
         ),
@@ -80,6 +100,6 @@ def match_client_with_therapists(
     return ClientShort(**form.__dict__).dict(), list(
         map(
             lambda item: load_therapist_media(db, item),
-            matches[last_index : limit + last_index],
+            matched_therapists[last_index : limit + last_index],
         )
     )
