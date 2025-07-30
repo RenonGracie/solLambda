43636b07fcf95ae91f4c1ded6e3500fb682afc55
renogra <renon.gracie@gmail.com>
2025-07-30 17:34:51 -0500

    payment type migration

--- migrations/env.py
+++ migrations/env.py
@@ -15,7 +15,7 @@ from src.models.db.airtable import AirtableTherapist # Import other models you w
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
+ 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
